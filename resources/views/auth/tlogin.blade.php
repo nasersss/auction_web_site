51fbbaa0e8107a5acec0ed5a088af6ed215da465
Abdulrahman Alsanaani <amas701111367@gmail.com>
2022-04-19 02:13:49 +0300

    smart

--- resources/views/auth/tlogin.blade.php
+++ resources/views/auth/tlogin.blade.php
@@ -5,6 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+                <!-- ********************************* -->
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
@@ -12,6 +13,7 @@
                         @csrf
 
                         <div class="row mb-3">
+                            <!-- ********************************* -->
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
                             <div class="col-md-6">
