b7545ffa7e442d8cdf90d6b4727027328b402b6e
Nasser Esmlail <eng.naser2022@gmail.com>
2022-04-18 13:22:45 +0300

    Procfile for Heroku

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/header', function () {
     return view('layout.header');
