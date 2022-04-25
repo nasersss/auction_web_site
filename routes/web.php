d228253886f41bacdd03da61a64a9c9c8ad80f22
Naser_Al-Ghaithi <74117992+nasersss@users.noreply.github.com>
2022-04-25 16:41:53 +0300

    adding rout of admin and super admin

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login',function(){
+    return view('auth.login');
+});
+
+Route::get('/register',function(){
+    return view('auth.register');
+});
+Route::get('/reset',function(){
+    return view('auth.reset');
+});
 Route::get('/dashboard', function () {
     return view('admin/dashboard');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin');
+
+Route::get('/superAdmin', [App\Http\Controllers\SuperAdminController::class, 'index'])->name('superAdmin');
