64a9ea1e26a2830f21ca9bff8fe24930232357b6
mohammed zubair <moh.775123554@gmail.com>
2022-05-08 16:59:23 +0300

    cread policies

--- routes/web.php
+++ routes/web.php
@@ -86,11 +86,11 @@ Route::get('/superAdmin', [SuperAdminController::class, 'index'])->name('superAd
 // Route::get('/uploadFileCategory', [App\Http\Controllers\CategoryController::class, 'uploadFile'])->name('uploadFileCategory');
 
 ########## polices route #######
-Route::get('/list_policies',[PoliciesController::class,"listPolicies"])->name("list_policies");
-Route::get('/add_policies',[PoliciesController::class,"addPolicies"])->name("add_policies");
-Route::post('/save_policies',[PoliciesController::class,"store"])->name("save_policies");
-Route::get('edit_policies/{PoliceId}',[PoliciesController::class,'edit'])->name('edit_policies');
-Route::post('update_policies/{PoliceId}',[PoliciesController::class,'update'])->name('update_policies');
-Route::get('toggle_policies/{PoliceId}',[PoliciesController::class,'toggle'])->name('toggle_policies');
+// Route::get('/list_policies',[PoliciesController::class,"listPolicies"])->name("list_policies");
+// Route::get('/add_policies',[PoliciesController::class,"addPolicies"])->name("add_policies");
+// Route::post('/save_policies',[PoliciesController::class,"store"])->name("save_policies");
+// Route::get('edit_policies/{PoliceId}',[PoliciesController::class,'edit'])->name('edit_policies');
+// Route::post('update_policies/{PoliceId}',[PoliciesController::class,'update'])->name('update_policies');
+// Route::get('toggle_policies/{PoliceId}',[PoliciesController::class,'toggle'])->name('toggle_policies');
 
 
