487d9d9c3bb95d18410cbfd71e9f3ea625f9a7a1
Abdulrahman Alsanaani <amas701111367@gmail.com>
2022-05-10 15:58:25 +0300

    add some updates to zubair

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = 'create_profile';
 
     /**
      * Create a new controller instance.
