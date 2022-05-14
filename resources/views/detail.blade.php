5b9737b21efa60015f1cf08e60c9189a7ee68a4d
Abdulrahman Alsanaani <amas701111367@gmail.com>
2022-05-14 13:21:02 +0300

    aaaaaaaaa

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="ظassets/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
 
     <link rel="stylesheet" href="/assets/css/ionicons.min.css">
     <link rel="stylesheet" href="/assets/css/all.css">
